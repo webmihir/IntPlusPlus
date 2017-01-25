9cd583a4cde0b0d06dcecafa3232da35a90ec16c
Mihir Gandhi <webmihir@gmail.com>
2017-01-25 13:09:25 -0800

    Fix the Leaky Bucket failing test scenario and add unit test for it

--- src/com/webmihir/IntPlusPlus/impl/impl2016/box/LeakyBucket.java
+++ src/com/webmihir/IntPlusPlus/impl/impl2016/box/LeakyBucket.java
@@ -1,17 +1,20 @@
 package com.webmihir.IntPlusPlus.impl.impl2016.box;
 
 import java.time.Clock;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 
 public class LeakyBucket {
   private final long _capacity;
   private long _numRequestsPending;
-  private long _lastTimeStamp;
-  private final int _rateNumerator;
-  private final int _rateDenominator;
+  private long _balanceTicks;
+  private long _lastTimeStamp = -1;
+  private final long _rateNumerator;
+  private final long _rateDenominator;
   private final Clock _clock;
 
-  public LeakyBucket(long capacity, int rateNumerator, int rateDenominator, Clock clock) {
+  public LeakyBucket(long capacity, long rateNumerator, long rateDenominator, Clock clock) {
     assert capacity > 0;
     assert rateDenominator > 0;
     assert rateNumerator > 0;
@@ -23,16 +26,30 @@ public class LeakyBucket {
     _clock = clock;
   }
 
+  public LeakyBucket(long capacity, long rate, Duration rateOver, Clock clock) {
+    assert capacity > 0;
+    assert rate > 0;
+    assert rateOver != null && rateOver.toMillis() > 0;
+    assert clock != null;
+
+    this._capacity = capacity;
+    this._rateNumerator = rate;
+    this._rateDenominator = rateOver.toMillis();
+    _clock = clock;
+  }
+
   public boolean canServeRequest() {
     long currentTime = _clock.millis();
-    long timeElapsed = currentTime - _lastTimeStamp;
+    long timeElapsed = currentTime - _lastTimeStamp + _balanceTicks;
 
-    //Remove served requests from the pending bucket
-    if (timeElapsed >= _rateDenominator) {
+    if (_lastTimeStamp == -1) {
+      _lastTimeStamp = currentTime;
+    } else if (timeElapsed >= _rateDenominator) {
       long requestsServed = (timeElapsed / _rateDenominator) * _rateNumerator;
+      _balanceTicks = (timeElapsed % _capacity);
+
       _numRequestsPending = Math.max(0, _numRequestsPending - requestsServed);
       _lastTimeStamp = currentTime;
-      System.out.println("Setting lastTimeStamp = " + _lastTimeStamp + " at " + currentTime);
     }
 
     //See if we can serve additional requests now
--- test/com/webmihir/IntPlusPlus/box/TestLeakyBucket.java
+++ test/com/webmihir/IntPlusPlus/box/TestLeakyBucket.java
@@ -4,6 +4,7 @@ import com.webmihir.IntPlusPlus.impl.impl2016.box.LeakyBucket;
 import java.time.Clock;
 import static org.mockito.Mockito.*;
 
+import java.time.Duration;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -18,7 +19,39 @@ public class TestLeakyBucket {
   @Test
   public void testRateLimiter() throws Exception {
     //Create a rate limiter for 1 requests per 3 millis
-    LeakyBucket testClass = new LeakyBucket(3, 1, 3, _mockClock);
+    LeakyBucket testClass = new LeakyBucket(3L, 1L, Duration.ofMillis(3L), _mockClock);
+
+    long currentMillis = System.currentTimeMillis();
+    //t = 1, send 3 requests
+    when(_mockClock.millis()).thenReturn(1L);
+    for (int i = 0; i < 3; i++) Assert.assertTrue(testClass.canServeRequest()); //3 requests in first second
+
+    //t = 2, send 1 request
+    when(_mockClock.millis()).thenReturn(2L);
+    Assert.assertFalse(testClass.canServeRequest());
+
+    //t = 3, send 1 request
+    when(_mockClock.millis()).thenReturn(3L);
+    Assert.assertFalse(testClass.canServeRequest());
+
+    //t = 4, send 1 request - should pass, 2nd request should fail
+    when(_mockClock.millis()).thenReturn(4L);
+    Assert.assertTrue(testClass.canServeRequest());
+    Assert.assertFalse(testClass.canServeRequest()); //2nd request should fail
+
+    //t = 5, send 1 request
+    when(_mockClock.millis()).thenReturn(5L);
+    Assert.assertFalse(testClass.canServeRequest());
+
+    //t = 6, send 1 request, should pass
+    when(_mockClock.millis()).thenReturn(6L);
+    Assert.assertFalse(testClass.canServeRequest());
+  }
+
+  @Test
+  public void testRateLimiter2() throws Exception {
+    //Create a rate limiter for 1 requests per 3 millis
+    LeakyBucket testClass = new LeakyBucket(3L, 1L, Duration.ofMillis(3L), _mockClock);
 
     long currentMillis = System.currentTimeMillis();
     //t = 1, send 3 requests
@@ -29,21 +62,12 @@ public class TestLeakyBucket {
     when(_mockClock.millis()).thenReturn(currentMillis + 2);
     Assert.assertFalse(testClass.canServeRequest());
 
-    //t = 3, send 1 request
-    when(_mockClock.millis()).thenReturn(currentMillis + 3);
-    Assert.assertFalse(testClass.canServeRequest());
-
-    //t = 4, send 1 request - should pass, 2nd request should fail
-    when(_mockClock.millis()).thenReturn(currentMillis + 4);
-    Assert.assertTrue(testClass.canServeRequest());
-    Assert.assertFalse(testClass.canServeRequest()); //2nd request should fail
-
-    //t = 5, send 1 request
+    //t = 5, send 1 request -- should pass
     when(_mockClock.millis()).thenReturn(currentMillis + 5);
-    Assert.assertFalse(testClass.canServeRequest());
+    Assert.assertTrue(testClass.canServeRequest());
 
-    //t = 6, send 1 request, should pass
-    when(_mockClock.millis()).thenReturn(currentMillis + 6);
-    Assert.assertFalse(testClass.canServeRequest());
+    //t = 7, send 1 request -- should pass
+    when(_mockClock.millis()).thenReturn(currentMillis + 7);
+    Assert.assertTrue(testClass.canServeRequest());
   }
 }
